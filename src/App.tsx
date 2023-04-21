27cfc51acd95d3261458dff86f06a3318f06797a
Mike Frazier <michael.s.frazier@gmail.com>
2023-04-21 08:46:05 -0600

    Add date-fns lib and create util file
    * Create form to test util functions

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,37 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import "./App.css";
+import { getDifferenceInWeeks, getMaxDate } from "./utils/date-helpers";
+
+const MAX_DATE = getMaxDate();
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [dob, setDob] = useState<Date | null>(null);
+  const [diffInWeeks, setDiffInWeeks] = useState<number | null>(null);
+
+  useEffect(() => {
+    if (dob !== null) {
+      setDiffInWeeks(getDifferenceInWeeks(dob));
+    } else {
+      setDiffInWeeks(null);
+    }
+  }, [dob]);
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
+      <h1>Life Chart</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+        <form>
+          <input
+            className="form-input"
+            type="date"
+            max={MAX_DATE}
+            onChange={(e) => setDob(e.target.valueAsDate)}
+          />
+        </form>
+        {diffInWeeks !== null && <h2>Weeks since birth: {diffInWeeks}</h2>}
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-      <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/utils/date-helpers.ts
@@ -0,0 +1,14 @@
+import differenceInWeeks from "date-fns/differenceInWeeks";
+import format from "date-fns/format";
+
+export const getToday = () => {
+    return new Date();
+}
+
+export const getMaxDate = () => {
+    return format(getToday(), 'yyyy-MM-dd')
+}
+
+export const getDifferenceInWeeks = (dateOfBirth: Date) => {
+    return differenceInWeeks(getToday(), dateOfBirth)
+}
