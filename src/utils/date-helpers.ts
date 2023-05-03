a992edb23d1666deb4072bd004a3f038de2b43db
Mike Frazier <michael.s.frazier@gmail.com>
2023-05-03 11:58:02 -0600

    Fix some of the date util issues

--- src/utils/date-helpers.ts
+++ src/utils/date-helpers.ts
@@ -5,10 +5,16 @@ export const getToday = () => {
     return new Date();
 }
 
+export const formatDate = (date: Date) => {
+    return format(date, 'yyyy/MM/dd')
+}
+
 export const getMaxDate = () => {
     return format(getToday(), 'yyyy-MM-dd')
 }
 
 export const getDifferenceInWeeks = (dateOfBirth: Date) => {
-    return differenceInWeeks(getToday(), dateOfBirth)
+    return differenceInWeeks(getToday(), dateOfBirth, {
+        roundingMethod: 'floor',
+    })
 }
